20d41dc21dcec72bf33ad43054b5649e7c293a9f
Javierdds <deduenassantanojavier@gmail.com>
2024-01-07 14:01:09 +0100

    refactor: score names now are readonly attributes of the class

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -6,15 +6,34 @@ namespace Tennis
     {
         private readonly string _player1Name;
         private readonly string _player2Name;
+        private readonly string[] _winningScoreNames;
+        private readonly string[] _advantageScoreNames;
+        private readonly string[] _runningScoreNames = 
+            { "Love", "Fifteen", "Thirty", "Forty" };
+        private readonly string[] _equalScoreNames = 
+            { "Love-All", "Fifteen-All", "Thirty-All", "Deuce" };
+        
         private int _score1;
         private int _score2;
-
+        
         public TennisGame1(string player1Name, string player2Name)
         {
             _player1Name = player1Name;
             _player2Name = player2Name;
+            
             _score1 = 0;
             _score2 = 0;
+            
+            _advantageScoreNames = new[]
+            {
+                "Advantage " + _player1Name,
+                "Advantage " + _player2Name
+            };
+            _winningScoreNames = new []
+            {
+                "Win for " + _player1Name,
+                "Win for " + _player2Name
+            };
         }
 
         public void WonPoint(string playerName)
@@ -28,11 +47,14 @@ namespace Tennis
             AddPointToPlayer2();
         }
 
-        private bool HasPlayer1WonThePoint(string playerName) => playerName == _player1Name;
+        private bool HasPlayer1WonThePoint(string playerName) 
+            => playerName == _player1Name;
 
-        private void AddPointToPlayer2() => _score2 += 1;
+        private void AddPointToPlayer2() 
+            => _score2 += 1;
 
-        private void AddPointToPlayer1() => _score1 += 1;
+        private void AddPointToPlayer1() 
+            => _score1 += 1;
 
 
         public string GetScore()
@@ -48,41 +70,16 @@ namespace Tennis
                 ? CheckAdvantageScoreValue() : CheckWinningScoreValue();
         }
 
-        private string CheckAdvantageScoreValue()
-        {
-            string[] scoreNames =
-            {
-                "Advantage " + _player1Name,
-                "Advantage " + _player2Name
-            };
+        private string CheckAdvantageScoreValue() 
+            => _score1 - _score2 == 1 ? _advantageScoreNames[0] : _advantageScoreNames[1];
 
-            return _score1 - _score2 == 1 ? scoreNames[0] : scoreNames[1];
-        }
+        private string CheckRunningScoreValue() 
+            => _runningScoreNames[_score1] + "-" + _runningScoreNames[_score2];
 
+        private string CheckWinningScoreValue() 
+            => _score1 - _score2 >= 2 ? _winningScoreNames[0] : _winningScoreNames[1];
 
-        private string CheckRunningScoreValue()
-        {
-            string[] scoreNames = { "Love", "Fifteen", "Thirty", "Forty" };
-
-            return scoreNames[_score1] + "-" + scoreNames[_score2];
-        }
-        
-        private string CheckWinningScoreValue()
-        {
-            string[] scoreNames =
-            {
-                "Win for " + _player1Name,
-                "Win for " + _player2Name
-            };
-
-            return _score1 - _score2 >= 2 ? scoreNames[0] : scoreNames[1];
-        }
-
-        private string CheckEqualScoreValue()
-        {
-            string[] scoreNames = { "Love-All", "Fifteen-All", "Thirty-All", "Deuce" };
-
-            return _score1 <= 2 ? scoreNames[_score1] : scoreNames[^1];
-        }
+        private string CheckEqualScoreValue() 
+            => _score1 <= 2 ? _equalScoreNames[_score1] : _equalScoreNames[^1];
     }
 }
